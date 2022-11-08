5ca97fad9abd486c83d4b470c1b5a0d34acd67b2
Prince01-encrypt <72964271+Prince01-encrypt@users.noreply.github.com>
2022-11-08 17:24:29 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -34,3 +34,35 @@ public:
         return false;
     }
 };
+
+
+
+******************************************************************** Using Count function in Set **********************************************************************
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head -> next == NULL) {
+            return false;
+        }
+        unordered_set<ListNode*> s1;
+        ListNode* temp = head;
+        
+        while(temp != NULL) {
+            temp = temp -> next;
+            if(s1.count(temp) == 1) {
+                return true;
+            }
+            s1.insert(temp);
+        }
+        return false;
+    }
+};
